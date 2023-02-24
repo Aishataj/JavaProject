6175ab715f0ec1b847268d6b403390be6beb21d4
Aishataj <aishataj786@yahoo.com>
2023-02-24 16:20:21 -0600

    first commit

---
+++ src/sample/JavaSample.java
@@ -0,0 +1,70 @@
+package sample;
+
+import java.util.Scanner;
+
+public class JavaSample {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hello World");
+
+        String nnn = "ahsia";
+        // lenght = 5-1
+
+        for (int g = nnn.length() - 1; g >= 0; g--) {
+            System.out.print(nnn.charAt(g));
+        }
+
+        {
+            // * * * * *
+            // * * * *
+            // * * *
+            // * *
+            // *
+        }
+        {
+
+            {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter the number of rows: ");
+                int n = sc.nextInt();
+                for (int s=np; i > 0; i--)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
+
+  package JavaRevision;
+
+  import java.util.ArrayList;
+  import java.util.Arrays;
+
+public class Polymorphism {
+
+    public void validateCountriesName() {
+        ArrayList list1 = new ArrayList(Arrays.asList("Pakistan", "Afghanistan", "India", "Bangladesh"));
+        ArrayList list2 = new ArrayList(Arrays.asList("Pakistan", "Afghanistan", "India", "Bangladesh"));
+
+
+        if (list1.equals(list2)) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+
+    }
+}
+ //github.com/Aishataj/JavaProject.git
+     // git init
+     // git add .
+     // git commit -m "first commit"
+     // git commit -m "first commit"
+     // git remote add origin https://github.com/Aishataj/JavaProject.git
+     // git push -u origin main
